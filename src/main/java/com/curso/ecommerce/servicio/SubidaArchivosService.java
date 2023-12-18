366350df1b7f330feebdd86a24555ae6c1abf3d6
Luchoromero29 <luchonromero@gmail.com>
2023-12-18 10:32:14 -0300

    Clase de servicio para subir una imagen al servidor

---
+++ src/main/java/com/curso/ecommerce/servicio/SubidaArchivosService.java
@@ -0,0 +1,34 @@
+package com.curso.ecommerce.servicio;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class SubidaArchivosService {
+
+	private String folder = "images//";
+	
+	//video 15
+	public String saveImage(MultipartFile file) throws IOException {
+		if (!file.isEmpty()) {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(folder + file.getOriginalFilename());
+			Files.write(path, bytes);
+			return file.getOriginalFilename();
+		}
+		return "default.jpg"; 
+	}
+	
+	//video 15
+	public void deleteImage(String nombre) {
+		String ruta = "image//";
+		File file = new File(ruta + nombre);
+		file.delete();
+	}
+}
